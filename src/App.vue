fffb74c24b1efd77c8992ed8f1e7741c8d1ff266
LeoneCarmelo <carmeloleone@outlook.it>
2023-06-13 16:11:13 +0200

    Add component + print on screen

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <script>
 import axios from 'axios';
+import CardProject from './components/CardProject.vue'
 
 export default {
+    components: {
+        CardProject
+    },
+    data() {
+        return {
+            projects: null,
+            loading: true,
+            base_url: 'http://127.0.0.1:8000/',
+            end_point_projects: 'api/projects'
+        }
+    },
+    methods: {
+        callAPI(url) { //endpoint projects
+            axios
+                .get(url)
+                .then(response => {
+                    console.log(response)
+                    this.projects = response.data.results.data
 
+                })
+                .catch(error => {
+                    console.log(error)
+                    console.log(error.message)
+                })
+        }
+    },
+    mounted() {
+        this.callAPI(this.base_url + this.end_point_projects)
+    }
 }
 </script>
 
 <template>
-
+    <div class="container">
+        <h1 class="text-center py-4">My Portfolio</h1>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3" v-if="projects">
+            <CardProject v-for="project in projects" :key="project.id" :image="project.image"
+                :link_project="project.link_project" :link_website="project.link_website" :description="project.description"
+                :technologies="project.technologies" />
+        </div>
+    </div>
 </template>
 
 <style lang="scss">
 @use './styles/general.scss';
-
-
 </style>
---
+++ src/components/CardProject.vue
@@ -0,0 +1,52 @@
+<script>
+export default {
+    name: 'CardProject',
+    props: {
+        image:String,
+        link_project: String,
+        link_website: String,
+        description: String,
+        technologies: Array,
+    },
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        cut_description(description) {
+            if (description.length > 100) {
+                return description.substring(0, 100)
+            }
+            return description
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="col">
+        <div class="card h-100">
+            <div class="card-img-top">
+                <img :src="image" alt="" class="img-fluid">
+            </div>
+            <div class="card-body">
+                <div class="link_project py-2">
+                    <strong>Link Project: </strong>
+                    <span>{{ link_project }}</span></div>
+                <div class="link_website py-2">
+                    <strong>Link Project: </strong>
+                    <span>{{ link_website }}</span>    
+                </div>
+                <p class="my-3">{{ cut_description(description) }}</p>
+            </div>
+            <div class="card-footer d-flex">
+                <span v-for="technology in technologies" :key="technology.id" class="mx-1">{{ technology.name }}</span>
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped></style>
